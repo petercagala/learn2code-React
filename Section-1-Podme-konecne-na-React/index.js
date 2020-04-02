c081cb30998788226229da092d6587482e84927d
Peter Čagala <petercagala@gmail.com>
2020-04-02 13:11:40 +0200

    [6-2] $r.setState(...)  ; react | class komponent, JSX, stav/state cez setState

--- Section-1-Podme-konecne-na-React/index.js
+++ Section-1-Podme-konecne-na-React/index.js
@@ -3,12 +3,27 @@
 
 // Tymto mam v apke novy html tag App
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dude: 'Marceline the vampire'
+        };
+    }
+
+
+    /**
+     * My template
+     */
     render() {
         // Toto nieje HTMLAllCollection, ale JSX code
         return (
-            <div onChange="nieco" tabIndex="nieco">
-                <p className="gule">
-                    Hello React
+            <div>
+                <p>
+                     My good friend <strong>{this.state.dude}</strong>
+                     <br/>
+                     I like {this.state.dude}
                 </p>
             </div>
         );
