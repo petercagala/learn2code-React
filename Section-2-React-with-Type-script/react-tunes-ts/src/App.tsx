d1c51761a6be0ec2afdee8af23508ee96dd36326
Peter Čagala <petercagala@gmail.com>
2020-04-07 15:34:24 +0200

    [40-3] React Router

--- Section-2-React-with-Type-script/react-tunes-ts/src/App.tsx
+++ Section-2-React-with-Type-script/react-tunes-ts/src/App.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
 import logo from './img/logo.svg';
 import './App.scss';
+import { Switch, Link, NavLink, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      {/* NavLink je specialna verzia Link komponentu 
+      NavLink: pridava class="active", ak sme na vygenerovanej podstranke
+      */}
+        <nav>
+          <NavLink to='/' exact>Home Page</NavLink>
+          <NavLink to='/tunes'>Tunes</NavLink>
+          <NavLink to='/about'>About</NavLink>
+        </nav>
       </header>
+
+      {/* Hlavny obsah podstranky sa ma zobrazit tu: importnem Rout, ktory vie reagovat na adresu:
+          Bud tam bachnem dalsi komponent, alebo nejaky vyrenderujem
+      */}
+      <main className="content">
+      {/* Switch zobrazi prvu, pre ktoru podmienka plati, cize musis pridat exact atribut 
+      exact : znamena, ze len vtedy, ak je to presne cesta, teda v tomto pripdate presne lomitko
+      ak v adrese chcem zachytit aj nieco ine, ako znenie adresy, nepouzi exact
+      */}
+        <Switch>
+           <Route path="/" render={() => <h1>Home page</h1>} exact></Route>
+            <Route path="/tunes" render={() => (
+                <div>
+                  <p>
+                      <Link to="/">Go Home</Link>
+                  </p>
+                  <p>
+                    <a href="/">Go home with refresh: a  element</a>
+                  </p>
+                  <h1>Tunes</h1>
+                </div>
+            )
+            }></Route>
+          <Route path="/about" render={() => <h1>About</h1>}></Route>
+        </Switch>
+      </main>
+
+      <footer>
+         <img src={logo} className="App-logo" alt="logo" />
+      </footer>
     </div>
   );
 }
--- Section-2-React-with-Type-script/react-tunes-ts/src/index.tsx
+++ Section-2-React-with-Type-script/react-tunes-ts/src/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// react router
+import {BrowserRouter, HashRouter} from 'react-router-dom'
+
 import './index.scss';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
+// Obal celu apku do BrowserRouter, vsade v nej budem moct pouzivat dalsie komponenty react-routera
+// Ak chces hashoidne  linky, pouzi HashRouter
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+    {/* <HashRouter>
+        <App/>
+    </HashRouter> */}
+  </React.StrictMode>
+  ,
   document.getElementById('root')
 );
 
